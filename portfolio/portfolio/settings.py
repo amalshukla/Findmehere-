70a4e61c13d5ecb8cebe6a460db4bb78b3fb99ac
Amal Shukla <amalshukla562000@gmail.com>
2026-03-23 01:38:12 +0530

    database switched

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -78,18 +78,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myportfoliodb',
-        'USER': 'root',
-        'PASSWORD': 'root1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
